eda69a1494809b72cdafb8b1f27fb11b1e3beb39
Massimo Morselli <massimo@morselli.com>
2024-01-07 21:41:03 +0100

    Add check for .env file existence in 1co.bat

--- 1co.bat
+++ 1co.bat
@@ -1,9 +1,15 @@
 @echo off
 cd /d %~dp0
 
+IF NOT EXIST .\1co\Configs\.env (
+    echo Please put your .env file in .\1co\Configs\.env
+    PAUSE
+    EXIT /B
+)
+
+
 REM uncomment the two line below to debug
 REM .\1co\Python\python.exe .\1co\App\main.py
 REM PAUSE
 
 START "" .\1co\Python\pythonw.exe .\1co\App\main.py
-
